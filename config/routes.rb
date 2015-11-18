646f1648842790f9fd8e2ef9c494ad88b379a2b2
Reece Rushing <rrushing715@gmail.com>
2015-11-18 10:14:50 -0500

    user can mark todos incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root "todos#index"
 
   resources :todos, only: [:index, :new, :create] do
-    resource :completion, only: [:create]
+    resource :completion, only: [:create, :destroy]
   end
-  
+
   resource :session, only: [:new, :create]
 end
